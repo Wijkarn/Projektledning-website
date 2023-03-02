c330c509e796f9c34358006d5fb790acff0b3148
Wijkarn <89905143+Wijkarn@users.noreply.github.com>
2023-03-02 10:56:52 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const select = document.querySelector("#select");
 const logInPageBtn = document.querySelector("#logInPageBtn");
 const formDiv = document.querySelector("#form");
 const profileBtn = document.querySelector(".profile");
+const publishBtn = document.querySelector("#publishButton");
+const articleContainer = document.querySelector("#articleContainer");
+const addArticle = document.querySelector("#addArticle");
 
 logInBtn.addEventListener("click", logIn);
 logInPageBtn.addEventListener("click", logInPagePopup);
 profileBtn.addEventListener("click", showProfilePage);
+publishBtn.addEventListener("click", publishArticle);
 
 let studentLoggedIn = false;
 let businessLoggedIn = false;
@@ -57,17 +61,69 @@ function logInPagePopup() {
 
 function showProfilePage() {
 
-    studentAdd = document.querySelector("#studentAdd");
-    articleContainer = document.querySelector("#articleContainer");
-    addArticle = document.querySelector("#addArticle");
-
-    if(studentLoggedIn){
-        studentAdd.style.display = "flex";
-        articleContainer.style.display = "none";
+    if (studentLoggedIn) {
+        studentUpload();
     }
-    else{
-        addArticle.style.display = "flex";
-        articleContainer.style.display = "none";
+    else {
+        busAddArticle();
     }
 
 }
+
+function studentUpload() {
+
+    const studentAdd = document.querySelector("#studentAdd");
+
+    studentAdd.style.display = "flex";
+    articleContainer.style.display = "none";
+
+}
+
+function busAddArticle() {
+
+    addArticle.style.display = "block";
+    articleContainer.style.display = "none";
+
+}
+
+function publishArticle(event) {
+    event.preventDefault();
+
+    const busName = document.querySelector("#articleHeaderInput").value;
+    const input = document.querySelector("#articleMainInput").value;
+    const publish = document.querySelector("#publishArticle").value;
+
+    if (publish == "direct" && busName.length != 0 && input.length != 0) {
+        const article = document.createElement("article");
+        const h2 = document.createElement("h2");
+        const main = document.createElement("p");
+        const time = document.createElement("p");
+
+        articleContainer.append(article);
+        article.append(h2);
+        article.append(main);
+        article.append(time);
+
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0');
+        let yyyy = today.getFullYear();
+
+        today = mm + '-' + dd + '-' + yyyy;
+
+        let d = new Date();
+        d.getHours();
+        d.getMinutes();
+
+        const output = "yeet";
+        output.concat(" ", d.getMinutes());
+
+        h2.innerText = busName;
+        main.innerText = input;
+        time.innerText = "Publisherad kl. ".concat(d.getHours(), ":", d.getMinutes(), " ", today);
+
+        addArticle.style.display = "none";
+        articleContainer.style.display = "flex";
+    }
+}
+
